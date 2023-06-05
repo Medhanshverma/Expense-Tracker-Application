091121f4e8e70b582358f2f5dda3c8c4fd331ab8
Medhansh Verma <medhanshv03@gmail.com>
2023-06-06 00:52:42 +0530

    Push
    Pushing Final Project

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors');
+const { db } = require('./db/db');
+const {readdirSync} = require('fs')
+const app = express()
+
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(cors())
+
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
+const server = () => {
+    db()
+    app.listen(PORT, () => {
+        console.log('listening to port:', PORT)
+    })
+}
+
+server()
